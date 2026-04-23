fe08d0b6881db4db4bbf5a3d10d2fbb0be83c94d
emefienem <emefienemmichael@gmail.com>
2026-04-23 08:26:36 +0100

    Applied new theme, improved auth screens and added logos

--- src/features/auth/components/register-form.tsx
+++ src/features/auth/components/register-form.tsx
@@ -90,6 +90,12 @@ export function RegisterForm() {
                     type="button"
                     disabled={isPending}
                   >
+                    <Image
+                      src="/logos/github.svg"
+                      width={20}
+                      height={20}
+                      alt="Github"
+                    />
                     Continue with Github
                   </Button>
                   <Button
@@ -98,6 +104,12 @@ export function RegisterForm() {
                     type="button"
                     disabled={isPending}
                   >
+                    <Image
+                      src="/logos/google.svg"
+                      width={20}
+                      height={20}
+                      alt="Google"
+                    />
                     Continue with Google
                   </Button>
                 </div>
